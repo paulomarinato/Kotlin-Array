c93c4e7669f4cd390c05dbf76cc66697552f48d3
paulomarinato <marinatopaulo@gmail.com>
2023-06-05 00:29:02 -0300

    Estudo de arrays boleanos

--- app/src/main/java/com/marinato/estudodearrays/EstudoDeArrays.kt
+++ app/src/main/java/com/marinato/estudodearrays/EstudoDeArrays.kt
@@ -1,19 +1,20 @@
 package com.marinato.estudodearrays
 
 fun main() {
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
 
-    val maiorIdade = if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4 ) {
-        idade1
-    }else if (idade2 > idade3 && idade2 > idade4) {
-        idade2
-    }else if (idade3 > idade4) {
-        idade3
-    }else{
-        idade4
+    val idades = IntArray(5)
+    idades[0] = 25
+    idades[1] = 19
+    idades[2] = 33
+    idades[3] = 20
+    idades[4] = 55
+
+    var maiorIdade = 0
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
     }
-    println("maiorIdade")
+
+    println(maiorIdade)
 }
