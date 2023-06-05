df745c8be1741c410821d75267000306fafb92f2
paulomarinato <marinatopaulo@gmail.com>
2023-06-05 00:30:41 -0300

    Estudando arrays intArraysOf

--- app/src/main/java/com/marinato/estudodearrays/EstudoDeArrays.kt
+++ app/src/main/java/com/marinato/estudodearrays/EstudoDeArrays.kt
@@ -2,12 +2,7 @@ package com.marinato.estudodearrays
 
 fun main() {
 
-    val idades = IntArray(5)
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
-    idades[4] = 55
+    val idades = intArrayOf(25, 19, 33, 20, 55)
 
     var maiorIdade = 0
     for (idade in idades) {
